5c5790e15d5f74a8cffb515053034f0e31169622
Ben <benjamin.stitt@gmail.com>
2018-09-19 11:42:13 -0400

    Add files via upload

---
+++ Computer Setup.ps1
@@ -0,0 +1,16 @@
+# Initial workspace setup
+
+# install Choco via powershell
+# make sure to run powershell as administrator
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# install software
+
+choco install vscode-powershell <#Visual Studio Code plus powershell extension#>, wireshark, angryip, firefox, git, putty <#SSH for Windows#>, python, windirstat, deluge <#torrent app#>, rufus, nmap, rdcman <#Remote Desktop Conneciton Manager#>, 
+# possibly required: webdeploy <#for IIS Web Server management#>, procexp <#process explorer#>, azure-cli, sql-server-management-studio,
+# 
+
+
+# upgrade packages
+choco upgrade all
