a41bafc0828974ecee64123cf3ef5cbf23dc2a4b
Ben <benjamin.stitt@gmail.com>
2019-04-17 21:11:37 -0400

    Update Computer Setup.ps1
    Added confirmation at end of line for chocolatey so I don't have to confirm any longer.

--- Computer Setup.ps1
+++ Computer Setup.ps1
@@ -7,7 +7,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 # install software
 
-choco install vscode-powershell <#Visual Studio Code plus powershell extension#>, wireshark, angryip, firefox, git, putty <#SSH for Windows#>, python, windirstat, deluge <#torrent app#>, rufus, nmap, rdcman <#Remote Desktop Conneciton Manager#>, 
+choco install vscode-powershell <#Visual Studio Code plus powershell extension#>, wireshark, angryip, firefox, git, putty <#SSH for Windows#>, python, windirstat, deluge <#torrent app#>, rufus, nmap, rdcman <#Remote Desktop Conneciton Manager#> -y
 # possibly required: webdeploy <#for IIS Web Server management#>, procexp <#process explorer#>, azure-cli, sql-server-management-studio,
 # 
 
